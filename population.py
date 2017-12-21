7220861fefb8396fced9617d4dea1794e15eae8d
Amid Majd <amidmajd@gmail.com>
2017-12-21 14:00:45 +0330

    new selection score pool

--- population.py
+++ population.py
@@ -79,56 +79,41 @@ class main():
                     random_gen = np.random.choice(dna)
                 else:
                     cromosome[g] = random_gen
-            # break
+            break
         return cromosome
 
 
     def selection(self):
-        if self.score.max() - self.score.min() == 0:
-            raise ValueError('Bad Population')
-        # print((self.score.max() - self.score.min()))
-        reg_score = (self.score - self.score.min()) / (self.score.max() - self.score.min())
-
         self.new_generation = []
-        top_len = self.pop_len // (self.dna_n) * 2
-        co_pop_len = self.pop_len - top_len
+        # top_len = self.pop_len // (self.dna_n) * 2
+        # co_pop_len = self.pop_len - top_len
 
-        tmp_score = []
-        for i,c in enumerate(self.pop):
-            tmp_score.append([i, self.fitness(c)])
-        tmp_score = sorted(tmp_score, key= lambda x: x[1], reverse=True)
+        # tmp_score = []
+        # for i,c in enumerate(self.pop):
+        #     tmp_score.append([i, self.fitness(c)])
+        # tmp_score = sorted(tmp_score, key= lambda x: x[1], reverse=True)
 
-        for i in range(top_len):
-            self.new_generation.append(self.pop[tmp_score[i][0]])
+        # for i in range(top_len):
+        #     self.new_generation.append(self.pop[tmp_score[i][0]])
 
-        self.score_pool = []
-        for i, item in enumerate(reg_score):
-            for j in range(int(np.floor(item * 100))):
-                self.score_pool.append(self.pop[i])
+        p = np.divide(self.score, self.score.sum())
 
-        self.score_pool = np.array(self.score_pool)
+        for i in range(self.pop_len // 2):
+            parent1 = self.pop[np.random.choice(range(self.pop_len), p=p)]
+            parent2 = self.pop[np.random.choice(range(self.pop_len), p=p)]
 
-        for i in range(co_pop_len // 2):
-            r1 = np.random.randint(0, len(self.score_pool))
-            r2 = np.random.randint(0, len(self.score_pool))
-            parent1 = self.score_pool[r1]
-            parent2 = self.score_pool[r2]
-
-            while( parent1 == parent2).all():
-                r1 = np.random.randint(0, len(self.score_pool))
-                r2 = np.random.randint(0, len(self.score_pool))
-                parent1 = self.score_pool[r1]
-                parent2 = self.score_pool[r2]
-                # print(r1, r2)
+            while (parent1 == parent2).all():
+                parent1 = self.pop[np.random.choice(range(self.pop_len), p=p)]
+                parent2 = self.pop[np.random.choice(range(self.pop_len), p=p)]
 
             child1, child2 = self.cross_over(parent1, parent2)
             # child1, child2 = self.mutation(child1), self.mutation(child2)
             self.new_generation.append(child1)
             self.new_generation.append(child2)
 
-        for i in range(self.pop_len - len(self.new_generation)):
-            r = np.random.randint(0, self.pop_len)
-            self.new_generation.append(self.pop[r])
+        # for i in range(self.pop_len - len(self.new_generation)):
+        #     r = np.random.randint(0, self.pop_len)
+        #     self.new_generation.append(self.pop[r])
 
         self.new_generation = np.array(self.new_generation)
         self.generation_num += 1
@@ -150,7 +135,7 @@ class main():
 
 if __name__ == '__main__':
     t0 = time.time()
-    population = main(16, 400, 0.02, 1000)
+    population = main(16, 50, 0.001, 1000)
     population.initialize()
     population.fitness_measure()
 
